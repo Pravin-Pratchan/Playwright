28be713a9fa743460d4b9e77cafa5866bbd35936
Pravin-Pratchan <spravinkumar010691@gmail.com>
2026-08-21 16:27:28 +0530

    1

--- tests/Homepage.spec.js
+++ tests/Homepage.spec.js
@@ -3,6 +3,7 @@ const {test, expect} = require('@playwright/test')
 test('HomePage', async ({page}) => {
   await page.goto('https://demoblaze.com/index.html');
   await expect(page).toHaveTitle('STORE');
+  await expect(page).toHaveURL('https://demoblaze.com/index.html');
   const a=await page.title();
   console.log(a);
   await page.close();
