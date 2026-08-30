2cd478704ed1432bb496ef37d0a67c9128658ad7
Pravin-Pratchan <spravinkumar010691@gmail.com>
2026-08-30 21:16:17 +0530

    Time out added in checkbox

---
+++ allure-results/c79caa44-d162-45cd-8cd6-dce65d61a530-attachment.md
@@ -0,0 +1,20 @@
+# Instructions
+
+- Following Playwright test failed.
+- Explain why, be concise, respect Playwright best practices.
+- Provide a snippet of code with the fix, if possible.
+
+# Test info
+
+- Name: checkbox.spec.js >> checkbox
+- Location: tests\checkbox.spec.js:2:1
+
+# Error details
+
+```
+Error: locator.check: Target page, context or browser has been closed
+```
+
+```
+Error: browserContext.close: Target page, context or browser has been closed
+```
--- tests/checkbox.spec.js
+++ tests/checkbox.spec.js
@@ -2,6 +2,9 @@ const {test, expect} = require ('@playwright/test')
 test('checkbox', async ({page})=>{
 
 await page.goto('https://testautomationpractice.blogspot.com/');
+await page.getByPlaceholder('Enter Name').fill("Pravin");
+await page.locator('#textarea').fill(`S. Pravinkumar, No.1/32D, Flat: G1, Perumal Koil street, Kolakappam, Chennai - 600128.`);
+await page.waitForTimeout(4000);
 await page.locator("//input[@id='sunday']").check();
 await expect (await page.locator("//input[@id='sunday']")).toBeChecked();
 await page.locator("//input[@id='monday']").check();
@@ -10,6 +13,7 @@ await page.locator("//input[@id='tuesday']").check();
 await expect (await page.locator("//input[@id='tuesday']")).toBeChecked();
 await page.locator("//input[@id='wednesday']").check();
 await expect (await page.locator("//input[@id='wednesday']")).toBeChecked();
+await page.waitForTimeout(4000);
 await expect (await page.locator("//input[@id='thursday']")).not.toBeChecked();
 await expect (await page.locator("//input[@id='friday']")).not.toBeChecked();
 await expect (await page.locator("//input[@id='saturday']")).not.toBeChecked();
