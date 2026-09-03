e84f6a258f3c614b44b94e3e7818941e9d15d210
Pravin-Pratchan <spravinkumar010691@gmail.com>
2026-09-03 15:55:31 +0530

    Changes made

--- tests/checkbox.spec.js
+++ tests/checkbox.spec.js
@@ -1,4 +1,4 @@
-const {test, expect} = require ('@playwright/test')
+import {test, expect} from '@playwright/test'
 test('checkbox', async ({page})=>{
 
 await page.goto('https://testautomationpractice.blogspot.com/');
